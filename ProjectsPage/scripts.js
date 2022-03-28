3d28c63d9ea2837baafecda87be6c602d6ba040f
Matheus <albuquerque12v@gmail.com>
2022-03-28 13:51:17 -0300

    Attempt to fix scale issues

--- ProjectsPage/scripts.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  landingPagefadeIn();
-};
-
-const landingPagefadeIn = () => {
-  var landingPage = document.getElementById("landing-page");
-  landingPage.classList = "landing-page-show";
-};
