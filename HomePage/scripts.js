f1cda6465230b234651f627af0d3f59a87e41cf8
Matheus <albuquerque12v@gmail.com>
2022-03-28 11:06:55 -0300

    Landing Page Basically finished and created/linked projects page

---
+++ HomePage/scripts.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  landingPagefadeIn();
+};
+
+const landingPagefadeIn = () => {
+  var landingPage = document.getElementById("landing-page");
+  landingPage.classList = "landing-page-show";
+};
