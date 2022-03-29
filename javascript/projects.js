512ad959b80925a7a61314da9c7da3a5dc61a964
Matheus <albuquerque12v@gmail.com>
2022-03-29 14:15:51 -0300

    Repo links in the project cards updated and funtional

---
+++ javascript/projects.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  showProjects();
+};
+
+const showProjects = () => {
+  var projectspage = document.getElementById("projects");
+  projectspage.classList = "projects-page-show";
+};
